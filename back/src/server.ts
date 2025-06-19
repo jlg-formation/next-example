6b175d73ea38b406c2458784ff1396ff3f6990ba
Jean-Louis GUENEGO <jlguenego@gmail.com>
2025-06-19 17:54:47 +0200

    fix: update development server script and change default port to 3333

--- back/src/server.ts
+++ back/src/server.ts
@@ -5,7 +5,7 @@ import serveIndex from "serve-index";
 import api from "./api";
 
 const app = express();
-const port = +(process.env.GSTOCK_PORT || 3000);
+const port = +(process.env.GSTOCK_PORT || 3333);
 const publicDir = "../front/dist";
 
 app.use((req, res, next) => {
