009fe0b0a77b98bb593b4e2136a4c084c2f26b91
Jean-Louis GUENEGO <jlguenego@gmail.com>
2025-06-19 15:25:45 +0200

    feat: Add SPA configuration to next.config.ts for static export and GitHub Pages support

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
+const type = "spa";
+
+const nextConfigForSPA: NextConfig = {
+  /* config options here */
+  output: "export", // Required for static export
+  trailingSlash: true, // Optional: helps for GitHub Pages routing
+  basePath: "/next-example", // Optional: if deploying to subpath (e.g. GitHub Pages)
+};
+
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+export default type === "spa" ? nextConfigForSPA : nextConfig;
