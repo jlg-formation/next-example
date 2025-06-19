34a4a70e951783ae0699e81c88a7e0c9352f30f1
Jean-Louis GUENEGO <jlguenego@gmail.com>
2025-06-19 15:48:46 +0200

    feat: Refactor next.config.ts to remove SPA type check and simplify export logic

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,15 @@
 import type { NextConfig } from "next";
 
-const type = "spa";
-
 const nextConfigForSPA: NextConfig = {
   /* config options here */
   output: "export", // Required for static export
   trailingSlash: true, // Optional: helps for GitHub Pages routing
   basePath: "/next-example", // Optional: if deploying to subpath (e.g. GitHub Pages)
 };
+console.log("nextConfigForSPA: ", nextConfigForSPA);
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default type === "spa" ? nextConfigForSPA : nextConfig;
+export default nextConfig;
