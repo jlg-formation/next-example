14b2557440083beab487b486b878f595c10aaaad
Jean-Louis GUENEGO <jlguenego@gmail.com>
2025-06-19 11:11:22 +0200

    fix: Correct logo alt text and adjust header link styling in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,14 @@ export default function Home() {
   return (
     <div className="flex flex-col min-h-screen justify-between">
       <header className="flex items-center bg-jlg-50 h-12">
-        <a href="#" className="flex items-center gap-2 px-2 hover:underline">
+        <a
+          href="#"
+          className="flex items-center gap-2 px-2 hover:underline h-10"
+        >
           <Image
             className="dark:invert"
             src="/logo.svg"
-            alt="Next.js logo"
+            alt="Gestion Stock Logo"
             width={16 * 2.5}
             height={16 * 2.5}
             priority
