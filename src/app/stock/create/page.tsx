1ee595523c593d237f96fbed89f6702cb78d2152
Jean-Louis GUENEGO <jlguenego@gmail.com>
2025-06-19 14:53:16 +0200

    feat: Enhance StockCreate form with validation and styling, and add form utility styles

--- src/app/stock/create/page.tsx
+++ src/app/stock/create/page.tsx
@@ -1,13 +1,33 @@
+import { PlusIcon } from "@heroicons/react/24/solid";
+
 export default function StockCreate() {
   return (
-    <main className="w-full max-w-3xl mx-auto">
+    <main className="w-full max-w-3xl mx-auto flex flex-col items-center">
       <h1 className="font-bold text-xl text-center my-8">Ajout d'un article</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae,
-        quas necessitatibus. Nostrum tempora aperiam sunt ipsum quibusdam
-        tenetur, delectus ex nihil beatae animi vero explicabo, alias dolores
-        quidem nemo deserunt!
-      </p>
+      <form className="form">
+        <label>
+          <span>Nom</span>
+          <input type="text" />
+          <span className="error">Champ obligatoire</span>
+        </label>
+        <label>
+          <span>Prix</span>
+          <input type="number" />
+          <span className="error"></span>
+        </label>
+        <label>
+          <span>Quantité</span>
+          <input type="number" />
+          <span className="error"></span>
+        </label>
+        <div className="h-12"></div>
+        <div className=" flex flex-col">
+          <button className="btn btn-primary">
+            <PlusIcon className="size-6" />
+            <span>Ajouter</span>
+          </button>
+        </div>
+      </form>
     </main>
   );
 }
